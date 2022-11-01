289886dab5eed248d25a40642b581265a9f03d8a
jasonkianhonting <jasonkianhonting@gmail.com>
2022-11-01 20:25:01 +1000

    Implemented a function to handle the user inputs from the get request function. Also added error handling for user inputs

--- task2.js
+++ task2.js
@@ -1,16 +1,57 @@
-// if (process.argv.length != 4) {
-// 	console.error("Expected two argument!");
-// 	process.exit(1);
-// }
 import fetch from "cross-fetch";
 
-async function fetchData() {
+async function fetchData(category, limit) {
+	let answerArr = [];
 	try {
-		let request = await fetch("https://api.publicapis.org/entries");
+		let request = await fetch(
+			`https://api.publicapis.org/entries?category=${category}`
+		);
 		let response = await request.json();
-		console.log(response);
+		let result = response.entries;
+		if (result != undefined || result != null) {
+			answerArr = answerArr.concat(result);
+			let sortedArr = answerArr.sort(function (initial, final) {
+				let initialItem = initial["API"].toLowerCase();
+				let finalItem = final["API"].toLowerCase();
+				return finalItem.localeCompare(initialItem);
+			});
+			let limitArr = sortedArr.slice(0, limit);
+			console.log(limitArr);
+		} else {
+			console.log("No results");
+		}
 	} catch (error) {
 		throw new Error(error.message);
 	}
 }
 
+function finalResult() {
+	try {
+		if (process.argv.length != 4) {
+			console.error("Expected two arguments: Category & Limit");
+			process.exit(1);
+		} else {
+			let userInput = process.argv.slice(2);
+			let categoryInput = userInput[0];
+			let limitInput = userInput[1];
+
+			const characterRegex = /^[A-Za-z]+$/;
+			const numberRegex = /^[0-9]+$/;
+			if (
+				categoryInput.match(characterRegex) &&
+				limitInput.match(numberRegex)
+			) {
+				let intLimitInput = parseInt(limitInput);
+				fetchData(categoryInput, intLimitInput);
+			} else {
+				console.log(
+					"Please input a valid category (alphabets) & a valid number (limit)"
+				);
+			}
+		}
+	} catch (err) {
+		throw new Error(err.message);
+	}
+}
+
+finalResult();
